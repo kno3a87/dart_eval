fb78083965f5a7273fb5f5088120d9986fd0283e
久野文菜 <ayana.kuno@mixi.co.jp>
2024-07-02 00:38:33 +0900

    add comment

--- lib/src/eval/runtime/ops/memory.dart
+++ lib/src/eval/runtime/ops/memory.dart
@@ -12,10 +12,13 @@ class PushArg implements EvcOp {
   // Set value at position to constant
   @override
   void run(Runtime runtime) {
+    print('ここに来てる？');
     if (_location < 0 || _location >= runtime.frame.length) {
+      print('_locationがマイナス！');
       // エラー処理または適切なデフォルト値の使用
       runtime.args.add(null); // または他の適切なデフォルト値
     } else {
+      print('ふつう！');
       runtime.args.add(runtime.frame[_location]);
     }
   }
